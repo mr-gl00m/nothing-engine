a62e3fbab4840a08575023872e65d961781f3d84
MR_GL00M <cidskullman1984@gmail.com>
2026-05-05 02:54:21 -0400

    Delete nothing_engine directory

--- nothing_engine/core/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Nothing Engine — Core simulation engine."""
-
-from .constants import HBAR, C, PI
-from .plate import DynamicalPlate
-from .energy_audit import PhysicalIntegrityError
-from .bogoliubov import SimulationConfig, SimulationResult, run_simulation
